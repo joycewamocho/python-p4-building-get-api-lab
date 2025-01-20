56fe3aae17454f5ad3b355e6dbaa867890f320ff
joyce wamocho <joywamo@gmail.com>
2025-01-20 20:23:55 +0300

    building get api

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,29 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    return make_response(jsonify(bakeries), 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_data = bakery.to_dict()
+    return make_response(jsonify(bakery_data),200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods =[]
+    for bakedgood in BakedGood.query.order_by(desc(BakedGood.price)).all():
+        baked_goods.append(bakedgood.to_dict())
+    return make_response(jsonify(baked_goods),200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    expensive_baked_good = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    return make_response(expensive_baked_good.to_dict(),200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
